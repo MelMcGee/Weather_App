60effb2d1757d976bacee84b3775b33e77d6392f
MelMcGee <mel@wecancodeit.org>
2015-11-16 01:03:28 -0500

    Initial updload of app. View the youtube video by We Can Code IT for a tutorial.

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            Weather weath = new Weather();
+            return Json(weath.getWeatherForcast(),JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string appid = "Change this to your APPID from openweathermap.org";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=" + appid + "&units=imperial";
+            //synchronous client.
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+    }
+}
